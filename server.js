5f920e3ef8cb07f3d3f51893304c60aa735bd7b6
moyarzo <66432546+moyarzo@users.noreply.github.com>
2026-03-28 16:57:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 SCADA corriendo en puerto", PORT);
 });
